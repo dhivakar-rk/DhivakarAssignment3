25b60836e2271ab33b14b9567e9f5227d4a603df
Dhivakar Radhakrishnan <divagar.3r@gmail.com>
2024-03-16 01:01:53 -0400

    Add files via upload

---
+++ test_area.py
@@ -0,0 +1,16 @@
+import pytest  
+from src.area import calculate_area_square  
+  
+def test_calculate_area_square_negative():  
+    with pytest.raises(TypeError):  
+        calculate_area_square(-2)  
+  
+def test_calculate_area_square_string():  
+    with pytest.raises(TypeError):  
+        calculate_area_square("2")  
+  
+def test_calculate_area_square_list():  
+    with pytest.raises(TypeError):  
+        calculate_area_square([2])
+def test_calculate_area_square_student_id():
+    assert calculate_area_square(8) == 64  # Assuming your student ID ends with 62
